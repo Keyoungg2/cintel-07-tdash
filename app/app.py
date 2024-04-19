599b1fe6352cb62027f330555e7ea702ae6d574c
Kareem <s568101@nwmissouri.edu>
2024-04-18 20:06:52 -0500

    Updated Code

--- app/app.py
+++ app/app.py
@@ -7,10 +7,10 @@ import palmerpenguins
 
 df = palmerpenguins.load_penguins()
 
-ui.page_opts(title="Penguins dashboard", fillable=True)
+ui.page_opts(title="K.young Penguins dashboard", fillable=True)
 
 
-with ui.sidebar(title="Filter controls"):
+with ui.sidebar(title="Filter controls", bg="blue"):
     ui.input_slider("mass", "Mass", 2000, 6000, 6000)
     ui.input_checkbox_group(
         "species",
@@ -21,18 +21,23 @@ with ui.sidebar(title="Filter controls"):
     ui.hr()
     ui.h6("Links")
     ui.a(
-        "GitHub Source",
+        "K. Young Project GitHub  Source",
+        href="https://github.com/Keyoungg2/cintel-07-tdash",
+        target="_blank",
+         )
+    ui.a(
+        "GitHub Original Project Source",
         href="https://github.com/denisecase/cintel-07-tdash",
         target="_blank",
-    )
+         )
     ui.a(
-        "GitHub App",
+        "GitHub Orginial App",
         href="https://denisecase.github.io/cintel-07-tdash/",
         target="_blank",
     )
     ui.a(
         "GitHub Issues",
-        href="https://github.com/denisecase/cintel-07-tdash/issues",
+        href="https://github.com/Keyoungg2/cintel-07-tdash/issues",
         target="_blank",
     )
     ui.a("PyShiny", href="https://shiny.posit.co/py/", target="_blank")
@@ -49,44 +54,48 @@ with ui.sidebar(title="Filter controls"):
 
 
 with ui.layout_column_wrap(fill=False):
-    with ui.value_box(showcase=icon_svg("earlybirds")):
-        "Number of penguins"
+    #Creating data value box for peguin count  
+    with ui.value_box(showcase=icon_svg("earlybirds"), theme="'text-blue', 'bg-gradient-blue-green'"):
+        "Number of penguins"#title of value box 
 
         @render.text
         def count():
             return filtered_df().shape[0]
 
-    with ui.value_box(showcase=icon_svg("ruler-horizontal")):
-        "Average bill length"
+    #Creating data value box for bill depth
+    with ui.value_box(showcase=icon_svg("ruler-horizontal"), theme="text-blue"):
+        "Average bill length"#title of value box 
 
         @render.text
         def bill_length():
             return f"{filtered_df()['bill_length_mm'].mean():.1f} mm"
 
-    with ui.value_box(showcase=icon_svg("ruler-vertical")):
-        "Average bill depth"
+    #Creating data value box for bill depth
+    with ui.value_box(showcase=icon_svg("ruler-vertical"), theme="text-blue"):
+        "Average bill depth" #title of value box 
 
         @render.text
         def bill_depth():
             return f"{filtered_df()['bill_depth_mm'].mean():.1f} mm"
 
-
-with ui.layout_columns():
-    with ui.card(full_screen=True):
-        ui.card_header("Bill length and depth")
-
-        @render.plot
-        def length_depth():
+with ui.navset_card_tab(id="tab"):
+    # Creating Scatter plot
+    with ui.nav_panel("Plotly scatterplot"):
+       ui.card_header("Bill length and depth") #Title for Frame
+       @render.plot
+       def length_depth():
             return sns.scatterplot(
                 data=filtered_df(),
                 x="bill_length_mm",
                 y="bill_depth_mm",
                 hue="species",
             )
+    with ui.nav_panel("Seaborn Histogram Chart"):
+        #Creating interactive data grid
+        ui.card(full_screen=True)
+        ui.card_header("Penguin data") #Title for Frame #Issue 1 corrected 
 
-    with ui.card(full_screen=True):
-        ui.card_header("Penguin da")
-
+        #Details columns c
         @render.data_frame
         def summary_statistics():
             cols = [
@@ -97,7 +106,6 @@ with ui.layout_columns():
                 "body_mass_g",
             ]
             return render.DataGrid(filtered_df()[cols], filters=True)
-
 
 #ui.include_css(app_dir / "styles.css")
 
